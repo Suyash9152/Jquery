dd3f24698c5b07ac777af263766e933caf2f8a4d
suyash9152 <suyashkadam2025@gmail.com>
2024-09-27 13:07:04 +0530

    new files added

---
+++ ex.js
@@ -0,0 +1,12 @@
+function fun1()
+{
+    document.write("This is function 1")
+}
+function fun2()
+{
+    document.write("This is function 2")
+}
+function fun3()
+{
+    document.write("This is function 3")
+}
